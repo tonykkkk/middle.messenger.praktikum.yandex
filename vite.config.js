901fdcd5099ada4e205e668659613f62c4809ad1
Anton Kulikov <tonyk@mail.ru>
2024-10-10 19:14:18 +0300

    netlify

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,7 @@ import handlebars from "vite-plugin-handlebars";
 export default defineConfig({
   plugins: [
     handlebars({
-      partialDirectory: [
-        resolve(__dirname, "src/partials"),
-        resolve(__dirname, "src/partials/pages"),
-        resolve(__dirname, "src/partials/forms"),
-        resolve(__dirname, "src/partials/elements"),
-      ],
+      partialDirectory: [resolve(__dirname, "src/partials")],
       reloadOnPartialChange: true,
     }),
   ],
